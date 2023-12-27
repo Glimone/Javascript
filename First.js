ae742b02782ab9a416aafb3e58876a1f23139ea2
Gabriel <glimone.nunes@outlook.com>
2023-12-27 15:12:03 -0300

    origin

--- First.js
+++ First.js
@@ -1,3 +1,9 @@
 //"Alert" é utilizado pra criar um pop-up.
 alert ("Hello world");
 alert ("O primeiro passo é esse!");
+
+
+// Existem 2 modos de fazer comentários em JS:
+// 1 - Utilizando "//", que se define apenas para uma linha.
+
+// 2- Utilizando "/**/", que se usa para além de uma linha.
