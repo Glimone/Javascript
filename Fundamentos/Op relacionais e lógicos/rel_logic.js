dc05d57007fcb2ff22247c7826d05fab58fe1964
Gabriel <glimone.nunes@outlook.com>
2023-12-28 18:35:04 -0300

    Operadores lógicos && e ||.

--- Fundamentos/Op relacionais e lógicos/rel_logic.js
+++ Fundamentos/Op relacionais e lógicos/rel_logic.js
@@ -23,3 +23,29 @@ let maioridade = 18;
 let resultado = idade > maioridade;
 console.log ("R:");
 console.log (resultado); 
+console.log ();
+
+console.log ("Exemplo2:");
+console.log ("Tenho 20 ovos e 5 quebraram, logo tenho 12.")
+let n1 = 20;
+let n2 = 5;
+let r = (n1 - n2) === 12;
+console.log (r);
+console.log ();
+
+console.log ("-----------------");
+//Operadores lógicos definem uma condição e levantam um true or false
+console.log ();
+console.log ("Tipos de operadores lógicos:");
+console.log ("E  = &&(Todos os elementos devem ser true para mostrar true)");
+console.log ("OU = ||(Apenas um elemento é suficiente para mostra true) ");
+console.log ();
+
+console.log ("Exemplo:");
+console.log ("Maria tem 20 anos, totalizando com Gabriel, ambos tem 41 anos");
+console.log ("Acho que a idade de Gabriel é 24!");
+let Maria = 20;
+let Gabriel = 21;
+let result = Maria === 20 && Gabriel === 22; 
+console.log (result);
+console.log ("Não! na verdade a idade dele é " + Gabriel);
