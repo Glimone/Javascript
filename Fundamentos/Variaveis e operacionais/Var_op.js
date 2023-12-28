8cf6da5a92dc073b9ec071d28c02552c4b8dda7c
Gabriel <glimone.nunes@outlook.com>
2023-12-28 14:53:37 -0300

    Explicação de definições de variáveis: let var const

---
+++ Fundamentos/Variaveis e operacionais/Var_op.js
@@ -0,0 +1,13 @@
+//Existem 3 tipos de definições de variáveis:
+    
+    let stringlet = "Let é utilizado para aplicação de escopo em bloco.";
+    console.log (stringlet); 
+    console.log () //Usado apenas para pular uma linha no terminal.
+
+    var stringvar = "Var é utilizado para aplicação de escopo geral. ";
+    console.log (stringvar) ; 
+    console.log ()
+
+    const stringconst = "Const é utilizado em caso da variável ter valor fixo.";
+    console.log (stringconst);
+    console.log ()
