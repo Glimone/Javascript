2f6ba9547dfa90d210f5087ff8acbfae3551d019
Gabriel <glimone.nunes@outlook.com>
2023-12-28 17:58:42 -0300

    Operadores lógicos.

---
+++ Fundamentos/Op relacionais e lógicos/rel_logic.js
@@ -0,0 +1,25 @@
+/*Operadores relacionais: 
+
+*São do tipo que relacionam os elementos e trazem um retorno booleano (False or true).
+*/
+
+console.log ("Tipos de operadores relacionais:");
+console.log ();
+console.log ("== - Igual a, comparando apenas os valores");
+console.log ("=== - Igual a, comparando os valores e o tipo de dado (Number, text)")
+console.log ("!= - Diferente de, comparando apenas os valores");
+console.log ("!== - Diferente de, comparando valores e o tipo de dado.");
+console.log ("> - Maior que");
+console.log ("< - Menor que");
+console.log (">= - Maior ou igual que");
+console.log ("<= - Menor ou igual que");
+console.log ();
+console.log ("Exemplos:");
+
+console.log ("A maioridade é alcançada aos 18");
+console.log ("Eu tenho 20, logo sou de maior");
+let idade = 20;
+let maioridade = 18;
+let resultado = idade > maioridade;
+console.log ("R:");
+console.log (resultado);
