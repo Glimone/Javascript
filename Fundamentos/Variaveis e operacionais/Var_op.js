b0be3ce251f7dfcc599c9e3338c61ea7d5f344d5
Gabriel <glimone.nunes@outlook.com>
2023-12-28 15:12:58 -0300

    Explicação de tipos de variáveis.

--- Fundamentos/Variaveis e operacionais/Var_op.js
+++ Fundamentos/Variaveis e operacionais/Var_op.js
@@ -1,5 +1,6 @@
 //Existem 3 tipos de definições de variáveis:
-    
+    console.log ("Definições de variáveis:");    
+
     let stringlet = "Let é utilizado para aplicação de escopo em bloco.";
     console.log (stringlet); 
     console.log () //Usado apenas para pular uma linha no terminal.
@@ -11,3 +12,28 @@
     const stringconst = "Const é utilizado em caso da variável ter valor fixo.";
     console.log (stringconst);
     console.log ()
+
+    console.log ("----------------------------")
+//Além disso, existem também 3 tipos de variáveis:
+    console.log ();
+    console.log ("Tipos de variáveis:")
+
+    let numero = "A primeira é do tipo número, que é dividida em 2 tipos:";
+    console.log (numero);
+    console.log ();
+
+    let decimal = "O do tipo decimal, como 5.50.";
+    console.log (decimal);
+    let inteiro = "O do tipo inteiro, como 10.";
+    console.log (inteiro);
+    console.log ();
+
+    let string = "Outra é do tipo texto, como esse próprio exemplo.";
+    console.log (string);
+    console.log ();
+
+    console.log ('Outro, é do tipo "verdadeiro ou falso".');
+    let boolean = true;
+    console.log (boolean);
+
+    /*Em resumo: Numéricas (int e dec), strings (texto) e booleans (true or false). */
