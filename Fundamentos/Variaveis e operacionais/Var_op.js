12a3cbf6ef9a21ff59c52dc4cc343801f1065a58
Gabriel <glimone.nunes@outlook.com>
2023-12-28 16:22:21 -0300

    Explicação dos operadores

--- Fundamentos/Variaveis e operacionais/Var_op.js
+++ Fundamentos/Variaveis e operacionais/Var_op.js
@@ -3,17 +3,17 @@
 
     let stringlet = "Let é utilizado para aplicação de escopo em bloco.";
     console.log (stringlet); 
-    console.log () //Usado apenas para pular uma linha no terminal.
+    console.log (); //Usado apenas para pular uma linha no terminal.
 
     var stringvar = "Var é utilizado para aplicação de escopo geral. ";
     console.log (stringvar) ; 
-    console.log ()
+    console.log ();
 
     const stringconst = "Const é utilizado em caso da variável ter valor fixo.";
     console.log (stringconst);
-    console.log ()
+    console.log ();
 
-    console.log ("----------------------------")
+    console.log ("----------------------------");
 //Além disso, existem também 3 tipos de variáveis:
     console.log ();
     console.log ("Tipos de variáveis:")
@@ -35,5 +35,26 @@
     console.log ('Outro, é do tipo "verdadeiro ou falso".');
     let boolean = true;
     console.log (boolean);
-
+    console.log ();
+    console.log ("----------------------------");
     /*Em resumo: Numéricas (int e dec), strings (texto) e booleans (true or false). */
+
+//Operadores
+
+    console.log ();
+    console.log ("Operadores:");
+    console.log ("O primeiro operador é a soma(+).");
+
+    let n1 = 10;
+    console.log ("O primeiro número é " + n1);
+    let n2 = 20;
+    console.log ("O segundo número é " + n2);
+
+    let soma = n1 + n2;
+    console.log ("A soma deles é " + soma);
+
+    console.log ();
+    console.log ("O mesmo se aplica com os outros, sendo:");
+    console.log ("Subtração (-)");
+    console.log ("Multiplicação (*)");
+    console.log ("Divisão (/)");
