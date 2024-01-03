dddfd47a3abe9f25c2582f702bbfc1a21066804a
Gabriel <glimone.nunes@outlook.com>
2024-01-03 14:44:08 -0300

    Projeto finalizado (Sem explicação)

--- Fundamentos/Projeto Etanol ou gasolina/calcule.js
+++ Fundamentos/Projeto Etanol ou gasolina/calcule.js
@@ -1,10 +1,27 @@
 
     
-    function preçoCombustivel () {
-    const Palcool = document.getElementById ('alcool') .value 
-    const Pgasolina = document.getElementById ('gasolina') .value 
-    let resultado = Palcool / Pgasolina 
+    function precoCombustivel () {
+        let palcool = document.getElementById ('alcool').value ;
+        let pgasosa = document.getElementById ('gasolina').value ;
+        let resultado = palcool / pgasosa
 
-    alert (Palcool)
+        if (palcool === "" && pgasosa === "") {
+            alert ("Escreva os valores dos combustíveis!")
+            document.getElementById ('result').innerHTML = "Resultado"
+        }else  if(pgasosa === "") {
+            alert ("Escreva o valor da gasolina") ;
+        } else if (palcool === "") {
+            alert ("Escreva os valores do Álcool");
+            
+        }
+           
+        if (resultado >= 0.7) {
+            document.getElementById ('result').innerHTML = "É melhor utilizar Gasolina"
+        } else if (resultado <0.7) {
+        document.getElementById ('result').innerHTML = "É melhor utilizar Álcool"
+        }else {
+            document.getElementById ('result').innerHTML = "Resultado"
+        }
     }
+
 
