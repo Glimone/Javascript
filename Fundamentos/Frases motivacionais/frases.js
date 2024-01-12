4c0b0f8900d124066e9c6568ba4bff52c2cc3ac8
root <glimone.nunes@outlook.com>
2024-01-11 23:48:56 -0300

    Explicações do projeto de Array

--- Fundamentos/Frases motivacionais/frases.js
+++ Fundamentos/Frases motivacionais/frases.js
@@ -8,7 +8,7 @@ function frasesNoAleatorio () { //Function conectada no Onclick
         "O corinthians não é um time que tem uma torcida, o corinthians é uma torcida que tem um time.",]; //var de arrays das frases.
  
     let aleatorio = Math.floor(Math.random() * 5) //var de valores aleatórios definidos pelo function math.random() e o número arredondado pelo math.floor. o "*5" define o limite de aleatóriedade, que deve ser baseado na quantidade de frases do array.
-    let faleatorio = strings[aleatorio] 
+    let faleatorio = strings[aleatorio]  //var criada pra receber var de frases (strings) e definir os arrays da mesma como aleatórios com a var "aleatório"
 
-    document.getElementById('frase').innerHTML = faleatorio
+    document.getElementById('frase').innerHTML = faleatorio //busca de ID aplicado no paragrafo das frases, para aplicar innerHTML, para que realize a alteração da frase direto no HTML. Definimos  o valor dessa mudança com o faleatorio, que seriam as frases ordanadas com array aleatório, conforme explicado acima. 
 }
