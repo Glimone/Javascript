c4af8234c743006283dca2472598a008b494daf7
root <glimone.nunes@outlook.com>
2024-01-12 14:13:26 -0300

    Atualização da aula de if_else, aplicando arrays]

--- Fundamentos/Estruturas condicionais/if_else.js
+++ Fundamentos/Estruturas condicionais/if_else.js
@@ -1,28 +1,47 @@
-console.log ("Meu IMC");
-console.log ("---------------");
-console.log ("Tenho 1.79m de altura");
-console.log ("Peso 77kg");
+function RetornoImc () {
 
-let altura = 1.79;
-let peso = 77;
+    let peso = document.getElementById('peso').value
+    let altura = document.getElementById('altura').value
+    
 
-let IMC = peso / (altura  ** 2);
+    let resultado = peso / altura**2
+    
 
-console.log ("Meu IMC é " + IMC);
+    
+    
+    let mensagem = [ 
+        "Você está muito abaixo do peso, procure ajuda!",
+        "Você está abaixo do peso, procure ajuda e se alimente melhor!",
+        "Você está no peso ideal",
+        "Você está um pouco acima do peso, se cuide um pouco mais!",
+        "Você está com obesidade N1, procure ajuda!",
+        "Você está com obesidade N2, procure ajuda imediatamente!"
+
+    ]
 
 
-if (IMC <= 16.9) {
-    console.log ("Estou muito abaixo do peso");
-} else if (IMC >= 17 && IMC <= 18.4) {
-    console.log ("Estou pouco abaixo do peso");
-} else if (IMC >= 18.5 && IMC <= 24.9) {
-    console.log ("Estou com o peso normal");
-} else if (IMC >= 25 && IMC <= 29.9) {
-    console.log ("Estou acima do peso");
-} else if (IMC >= 30 && IMC <= 34.9) {
-    console.log ("Estou muito acima do peso");
-} else if (IMC >= 35 && IMC <= 40) {
-    console.log ("Estou com obesidade n1");
-} else  {
-    console.log ("Estou com obesidade n2");
+
+
+    let retorno 
+
+    if (resultado < 16.9) {
+        retorno = mensagem[0]
+    } else if (resultado >= 17 &&  resultado <= 18.4 ) {
+        retorno = mensagem[1]
+    } else if (resultado >= 18.5 &&  resultado <= 24.9 ) {
+        retorno = mensagem[2]
+    } else if (resultado >= 25 &&  resultado <= 29.9 ) {
+        retorno = mensagem[3]
+    } else if (resultado >= 30 &&  resultado <= 34.4 ) {
+        retorno = mensagem[4]
+    } else if (resultado >= 35 &&  resultado <= 40 ) {
+        retorno = mensagem[5]
+    } else if (resultado > 40) {
+        retorno = mensagem[6]
+    } 
+    
+    
+
+
+    document.getElementById('retorno').innerHTML = retorno
 }
